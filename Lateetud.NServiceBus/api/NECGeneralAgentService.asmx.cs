6075a2719d11d7e5aae0d373c01278a1964d4bd9
Ripan Paul <ripan.paul@lateetud.com>
2017-12-01 19:59:19 +0530

    changes done for NECGeneralAgent

---
+++ Lateetud.NServiceBus/api/NECGeneralAgentService.asmx.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Web.Services;
+using System.Configuration;
+using Lateetud.NServiceBus.Common;
+using Lateetud.NServiceBus.Common.Models.NECGeneralAgent;
+
+namespace Lateetud.NServiceBus.api
+{
+    /// <summary>
+    /// Summary description for NECGeneralAgentService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class NECGeneralAgentService : System.Web.Services.WebService
+    {
+        MsmqSqlDBConfiguration msmqsqldbconfig = new MsmqSqlDBConfiguration(ConfigurationManager.ConnectionStrings["SqlPersistence"].ConnectionString);
+
+        [WebMethod]
+        public string CreatePublisherQueues()
+        {
+            var endpointConfiguration = msmqsqldbconfig.ConfigureEndpoint("NEC.GeneralAgent.Publisher");
+            msmqsqldbconfig.StartEndpoint(endpointConfiguration).GetAwaiter().GetResult();
+
+            return "Created publishers queues";
+        }
+
+        [WebMethod]
+        public string PublishNECGeneralAgent(string message)
+        {
+            var endpointConfiguration = msmqsqldbconfig.ConfigureEndpoint("NEC.GeneralAgent.Publisher");
+            return msmqsqldbconfig.PublishedToBus(endpointConfiguration, new NECGeneralAgent { Message = message });
+        }
+
+        [WebMethod]
+        public string PublishNECGeneralAgentResult(string message)
+        {
+            var endpointConfiguration = msmqsqldbconfig.ConfigureEndpoint("NEC.GeneralAgent.Publisher");
+            return msmqsqldbconfig.PublishedToBus(endpointConfiguration, new NECGeneralAgentResult { Message = message });
+        }
+
+    }
+}
